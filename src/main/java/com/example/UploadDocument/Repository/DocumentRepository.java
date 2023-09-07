cd8374e25a3ce07b8746ff46a9d61fd560361a1d
AKhil <akhilbhosale01999@gmail.com>
2023-09-07 11:08:49 +0530

    "first Commit"

--- src/main/java/com/example/UploadDocument/Repository/DocumentRepository.java
+++ src/main/java/com/example/UploadDocument/Repository/DocumentRepository.java
@@ -2,6 +2,8 @@ package com.example.UploadDocument.Repository;
 
 import com.example.UploadDocument.Bean.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
